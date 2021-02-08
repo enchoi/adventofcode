ad17e1597f6e8ef8e5c2c7f0edab2525793def24
Jason LETORT <jletort@aldebaran.com>
2021-02-08 17:11:57 +0100

    Part2

--- day6.py
+++ day6.py
@@ -8,7 +8,9 @@ G)H
 D)I
 E)J
 J)K
-K)L"""
+K)L
+K)YOU
+I)SAN"""
 
 
 class Planet:
@@ -32,6 +34,10 @@ class Planet:
         """ Add planet in obit """
         self.children[planet.name] = planet
 
+    def set_parent(self, planet):
+        """ who my mumy """
+        self.parent = planet
+
 
 def main(entry):
     # create planets
@@ -48,17 +54,54 @@ def main(entry):
 
     def compute(planet):
         for _, child in planet.children.items():
-            child.set_index(planet.index+1)
+            child.set_index(planet.index + 1)
+            child.set_parent(planet)
             compute(child)
 
     compute(com)
     orbits = sum([planet.index for planet in planets.values()])
-    return orbits
+    print(f"Number of total orbits : {orbits}")
+    return planets
+
+
+def find_path_to_com(planet):
+    """ find the path to COM """
+    planet = planet.parent
+    path = []
+    while planet.name != "COM":
+        path.append(planet.name)
+        planet = planet.parent
+
+    # add COM
+    path.append(planet.name)
+    return path[::-1]
+
+
+def find_closer_parent(planet1, planet2):
+    """ find the first relative """
+    path1 = find_path_to_com(planet1)
+    path2 = find_path_to_com(planet2)
+    for plt1, plt2 in zip(path1, path2):
+        if plt1 != plt2:
+            break
+        planet_joinction = plt1
+    return planet_joinction
+    # return (plt1.index - planet_joinction) + (plt2 - planet_joinction)
 
 
 if __name__ == '__main__':
-    # total_orbits = main(test)
+    # planets = main(test)
     with open("day6.input", "r") as fichier:
         data = fichier.read()
-    total_orbits = main(data)
-    print(total_orbits)
+    planets = main(data)
+    # print(total_orbits)
+    # find_path_to_com(planets["YOU"])
+    you = planets["YOU"]
+    santa = planets["SAN"]
+
+    joinction = find_closer_parent(you, santa)
+    joinction = planets[joinction]
+
+    distance = (you.index - joinction.index) + \
+        (santa.index - joinction.index) - 2
+    print(f"Orbits between you and Santa: {distance}")
