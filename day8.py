0dd936b159b440fbaeec8a6e0fe69fd5f3296dba
Jason LETORT <jletort@aldebaran.com>
2021-02-09 15:18:17 +0100

    day8 part1

---
+++ day8.py
@@ -0,0 +1,61 @@
+import numpy as np
+from itertools import chain
+
+
+class Elves_Image:
+    def __init__(self, width, height, data):
+        self.layers = []
+        self.data = data
+        self.width = width
+        self.height = height
+
+        # compute layers
+        self.compute()
+
+    def compute(self):
+        """ split data into multiple layers """
+        size = self.height * self.width
+        layers = [self.data[size*x:(x+1)*size]
+                  for x in range(int(len(self.data)/size))]
+        self.layers = [np.resize(list(layer), (self.height, self.width))
+                       for layer in layers]
+
+    def find(self, condition="fewest", number="0"):
+        """ find a layer """
+        if condition == "fewest":
+            rev = False
+        elif condition == "most":
+            rev = True
+        if isinstance(number, int):
+            number = str(number)
+
+        # strings = [self._to_string(layer), for layer in layers]
+        layer = sorted(self.layers,
+                       key=lambda x:
+                       self.to_string(x).count(number), reverse=rev)[0]
+
+        return layer
+
+    @staticmethod
+    def to_string(layer):
+        """ transform a layer into string """
+        return "".join(chain.from_iterable(layer.tolist()))
+
+
+def main():
+    """ the main test """
+    with open("day8.input", "r") as fichier:
+        data = fichier.read()
+
+    image = Elves_Image(25, 6, data)
+    layer = image.find()
+    layer_data = image.to_string(layer)
+    ones = layer_data.count("1")
+    twos = layer_data.count("2")
+    print(f"1: {ones}")
+    print(f"2: {twos}")
+    print(f"multiplication: {ones * twos}")
+
+
+if __name__ == '__main__':
+    main()
